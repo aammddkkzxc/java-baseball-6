f2909ab3aaf53ef3e5fe4f7ff8e38872e005243a
aammddkkzxc <ideal0427@naver.com>
2023-12-06 21:09:05 +0900

    refactor: 유저 넘버 숫자로 변환하는 기능 리팩터링

--- src/main/java/baseball/Converter.java
+++ src/main/java/baseball/Converter.java
@@ -6,9 +6,13 @@ import java.util.List;
 public class Converter {
     private static final String USER_NUMBER_RE_REQUEST_MESSAGE = "사용자 번호를 다시 입력해 주십시오.";
 
-    public static int convertUserNumberNumeric(String inputUserNumber) {
+    public static List<Integer> convertUserNumber(List<Character> separatedUserNumber) {
+        List<Integer> convertedUserNumber = new ArrayList<>();
         try {
-            return Integer.parseInt(inputUserNumber);
+            for(char number : separatedUserNumber) {
+                convertedUserNumber.add(Integer.parseInt(String.valueOf(number)));
+            }
+            return convertedUserNumber;
         } catch (NumberFormatException e) {
             throw new IllegalArgumentException(USER_NUMBER_RE_REQUEST_MESSAGE);
         }
