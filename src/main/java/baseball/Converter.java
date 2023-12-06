fa6e4d62bb9d1e19ac4ae21b6539ac386de17f6a
aammddkkzxc <ideal0427@naver.com>
2023-12-06 21:03:18 +0900

    feat: 입력받은 유저 넘버 자리수 별로 나누는 기능 구현

--- src/main/java/baseball/Converter.java
+++ src/main/java/baseball/Converter.java
@@ -1,13 +1,25 @@
 package baseball;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Converter {
     private static final String USER_NUMBER_RE_REQUEST_MESSAGE = "사용자 번호를 다시 입력해 주십시오.";
 
-    public static int convertUserNumber(String inputUserNumber) {
+    public static int convertUserNumberNumeric(String inputUserNumber) {
         try {
             return Integer.parseInt(inputUserNumber);
         } catch (NumberFormatException e) {
             throw new IllegalArgumentException(USER_NUMBER_RE_REQUEST_MESSAGE);
         }
+    }
+
+    public static List<Character> separateUserNumber(String userNumber) {
+        List<Character> separatedUserNumber = new ArrayList<>();
+        for(int i = 0; i<userNumber.length(); i++) {
+            separatedUserNumber.add(userNumber.charAt(i));
+        }
+
+        return separatedUserNumber;
     }
 }
