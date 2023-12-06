fba2d37b42249aa368531eb962dedb496b2d7868
aammddkkzxc <ideal0427@naver.com>
2023-12-06 21:31:57 +0900

    feat: 입력된 사용자 번호 3자리 수가 아닐 경우 예외 발생 기능 구현

--- src/main/java/baseball/UserNumber.java
+++ src/main/java/baseball/UserNumber.java
@@ -1,6 +1,8 @@
 package baseball;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class UserNumber {
     public static final String USER_NUMBER_RE_REQUEST_MESSAGE = "사용자 번호를 다시 입력해 주십시오.";
@@ -8,10 +10,16 @@ public class UserNumber {
     private final List<Integer> number;
 
     public UserNumber(List<Integer> number) {
+        validateDuplication(number);
+
         this.number = number;
     }
 
-    private void validateDuplication() {
+    private void validateDuplication(List<Integer> number) {
+        Set<Integer> duplicationChecker = new HashSet<>(number);
+        if(duplicationChecker.size() != number.size()) {
+            throw new IllegalArgumentException(USER_NUMBER_RE_REQUEST_MESSAGE);
+        }
     }
 
     private void validateNumberLength() {
